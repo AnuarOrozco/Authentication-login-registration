65e8cedee14e32e3374892dda654a0b510112bfb
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 13:11:38 -0600

    Created controller with the login view

---
+++ src/main/java/com/springsec/User_registry_spring_security/controller/RegisterController.java
@@ -0,0 +1,14 @@
+package com.springsec.User_registry_spring_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+}
