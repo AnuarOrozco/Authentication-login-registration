2ae75495dd63e197e1a83398c80b5917ae8ccf64
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 12:58:31 -0600

    Extended from UserDetailsService

--- src/main/java/com/springsec/User_registry_spring_security/service/IUserService.java
+++ src/main/java/com/springsec/User_registry_spring_security/service/IUserService.java
@@ -2,8 +2,9 @@ package com.springsec.User_registry_spring_security.service;
 
 import com.springsec.User_registry_spring_security.dto.UserRegistryDto;
 import com.springsec.User_registry_spring_security.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface IUserService {
+public interface IUserService extends UserDetailsService {
 
     public User saveUser(UserRegistryDto userRegistryDto);
 
