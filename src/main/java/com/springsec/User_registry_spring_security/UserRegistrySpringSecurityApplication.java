e861930bc843ff1aebc342edc6159e1f893766a5
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 11:44:32 -0600

    Init commit

---
+++ src/main/java/com/springsec/User_registry_spring_security/UserRegistrySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springsec.User_registry_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrySpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsec/User_registry_spring_security/UserRegistrySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec.User_registry_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrySpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
