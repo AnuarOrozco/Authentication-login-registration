70e6cc5dc57a1994a96af40440eba6e3030ec53b
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 13:09:19 -0600

    Implemented new methods including UserDetails

--- src/main/java/com/springsec/User_registry_spring_security/service/UserServiceImpl.java
+++ src/main/java/com/springsec/User_registry_spring_security/service/UserServiceImpl.java
@@ -5,9 +5,16 @@ import com.springsec.User_registry_spring_security.model.Role;
 import com.springsec.User_registry_spring_security.model.User;
 import com.springsec.User_registry_spring_security.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements IUserService{
@@ -25,4 +32,28 @@ public class UserServiceImpl implements IUserService{
                 Arrays.asList(new Role("ROLE_USER")));
         return userRepository.save(user);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuario o contrasena invalidos");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                mapRoleAuthority(user.getRoles())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> mapRoleAuthority(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<User> showAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
