d72d253d29476258e009043e9e96fd18ce8ec9d2
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 13:09:01 -0600

    Created new method showAllUsers

--- src/main/java/com/springsec/User_registry_spring_security/service/IUserService.java
+++ src/main/java/com/springsec/User_registry_spring_security/service/IUserService.java
@@ -4,8 +4,10 @@ import com.springsec.User_registry_spring_security.dto.UserRegistryDto;
 import com.springsec.User_registry_spring_security.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface IUserService extends UserDetailsService {
 
     public User saveUser(UserRegistryDto userRegistryDto);
-
+    List<User> showAllUsers();
 }
