8d983563f94b5c3354237326dc78a71943f6a8f5
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 12:02:22 -0600

    Implemented the relation between classes in the db

--- src/main/java/com/springsec/User_registry_spring_security/model/User.java
+++ src/main/java/com/springsec/User_registry_spring_security/model/User.java
@@ -22,6 +22,13 @@ public class User {
 
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
     private Collection<Role> roles;
 
 }
